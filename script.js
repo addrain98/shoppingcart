742de159719ccf0e9a8ece935efccaa68e2659c3
Jia Yu Koh <kohjiayu1998@gmail.com>
2023-10-21 15:11:53 +0000

    completed addCart Function, doing display puchase cart

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    const addItemButton = document.querySelector("#addItem");
-    addItemButton.addEventListener('click',function() {
-        const itemNameInput = item.name;
-        const itemQuantityInput = item.quantity;
-        const itemPriceInput = item.price;
-        const itemTotalPriceInput = item.total;
-    })
-    
-    function renderItems(shoppingCart) {
-        const container = document.getElementById("product-cards");
+    function addToCart(itemID){
+        let product = null;
+
+        for (let i = 0; i < product.length; i++){
+            if (product[i].id === itemID){
+                product = product[i];
+                break;
+            }
+        }
+
+        if (product) {
+            let cartItem = null;
+            for (let i = 0; i < cart.length; i++) {
+                if (cart[i].id === itemID) {
+                    cartItem = cart[i];
+                    break
+                }
+            }
+        }
+
+        if (cartItem) {
+            cartItem.quantity += 1;
+        }
+
+        else {
+            cart.push({...product, quantity: 1 })
+        }
+
         
-        for (let item of shoppingCart) {
+    }
+
+    function updateDisplayCart() {
+        const cartContainer = document.getElementById("purchase-cart");
+        cartContainer.innerhtml = '';
+        for (let item of cart) {
+            const listItem = document.createElement('li');
+            
+            console.log(`Name: ${item.name}, Quantity: ${item.quantity}, Price: ${item.price}`)
+        }
+    }
+    
+    function renderProducts(items) {
+        const cardsContainer = document.getElementById("product-cards");
+        
+        for (let item of items) {
             const card = document.createElement("div");
             card.classList.add("col-md-4", "mb-4");
         
